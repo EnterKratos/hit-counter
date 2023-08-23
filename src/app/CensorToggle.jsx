4616bf58a003711b7a09ad9e7508d1e19047183c
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2023-08-23 18:16:13 +0100

    Add CensorToggle client component

---
+++ src/app/CensorToggle.jsx
@@ -0,0 +1,16 @@
+﻿'use client';
+import React from 'react';
+
+function CensorToggle({ children }) {
+  const [censored, setCensored] = React.useState(true);
+
+  function handleClick(e) {
+    setCensored(!censored);
+  }
+
+  return (
+    <button onClick={handleClick} className={censored ? 'censored' : undefined} >{children}</button>
+  )
+}
+
+export default CensorToggle;
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CensorToggle from "./CensorToggle";
 
 import {
   logSiteVisit,
@@ -6,10 +7,11 @@ import {
 
 function Home() {
   const visitCount = logSiteVisit();
+
   return (
     <main>
       <h1>Welcome!</h1>
-      <p>You are visitor number {visitCount}.</p>
+      <p>You are visitor number <CensorToggle>{visitCount}</CensorToggle>.</p>
     </main>
   );
 }
