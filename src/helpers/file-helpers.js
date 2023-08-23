e3e8faa5bfe2ffba8aba6703ad3a2d9247356557
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2023-08-23 17:48:43 +0100

    Add naive hit counter

--- src/helpers/file-helpers.js
+++ src/helpers/file-helpers.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 
+const DATABASE_PATH = '/src/database.json';
+
 export function readFile(localPath) {
   return fs.readFileSync(
     path.join(process.cwd(), localPath),
@@ -14,4 +16,12 @@ export function writeFile(localPath, content) {
     content,
     { encoding: 'utf8' }
   );
+}
+
+export function logSiteVisit() {
+  const db = JSON.parse(readFile(DATABASE_PATH));
+  db.hits += 1;
+  writeFile(DATABASE_PATH, JSON.stringify(db));
+
+  return db.hits;
 }
